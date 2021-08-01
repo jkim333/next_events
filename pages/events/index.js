fd77d4d52ebfe27b296d4ef6c2e64cf1b47b4117
Jihyung Kim <kimjihyung3@gmail.com>
2021-08-01 15:23:48 +1000

    Allowed navigation to filtered events page.

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,11 +1,19 @@
 import { getAllEvents } from '../../dummy-data';
 import EventList from '../../components/events/event-list';
+import EventsSearch from '../../components/events/events-search';
+import { useRouter } from 'next/router';
 
 function AllEventsPage() {
   const events = getAllEvents();
+  const router = useRouter();
+
+  const findEventsHandler = (year, month) => {
+    router.push(`/events/${year}/${month}`);
+  };
 
   return (
     <div>
+      <EventsSearch onSearch={findEventsHandler} />
       <EventList items={events} />
     </div>
   );
