2ff7bb5e093546557b3332f66a43d00b5bf4fa50
Jihyung Kim <kimjihyung3@gmail.com>
2021-09-01 13:55:41 +1000

    Updated events detail page to fetch data using static generation.

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,23 +1,11 @@
 import { Fragment } from "react";
-import { useRouter } from "next/router";
-import { getEventById } from "../../dummy-data";
 import EventSummary from "../../components/event-detail/event-summary";
 import EventLogistics from "../../components/event-detail/event-logistics";
 import EventContent from "../../components/event-detail/event-content";
-import ErrorAlert from "../../components/ui/error-alert";
+import { getEventById, getFeaturedEvents } from "../../helpers/api-util";
 
-function EventDetailPage() {
-  const router = useRouter();
-  const { eventId } = router.query;
-
-  const event = getEventById(eventId);
-  if (!event) {
-    return (
-      <ErrorAlert>
-        <p>No event found!</p>
-      </ErrorAlert>
-    );
-  }
+function EventDetailPage(props) {
+  const event = props.event;
 
   return (
     <Fragment>
@@ -33,6 +21,35 @@ function EventDetailPage() {
       </EventContent>
     </Fragment>
   );
+}
+
+export async function getStaticProps(context) {
+  const { params } = context;
+  const eventId = params.eventId;
+  const event = await getEventById(eventId);
+
+  if (!event) {
+    return {
+      notFound: true,
+    };
+  }
+
+  return {
+    props: {
+      event: event,
+    },
+    revalidate: 30,
+  };
+}
+
+export async function getStaticPaths() {
+  const events = await getFeaturedEvents();
+  const paths = events.map((event) => ({ params: { eventId: event.id } }));
+
+  return {
+    paths: paths,
+    fallback: "blocking",
+  };
 }
 
 export default EventDetailPage;
